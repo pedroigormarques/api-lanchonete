88420b4007c260e8f3cdf91278e4b718832c4ca2
pedroigormarques <pedroigor.marques@gmail.com>
2023-03-04 13:43:29 -0300

    Início da criação do sistema de erros geral

---
+++ src/exception/exception-filter.spec.ts
@@ -0,0 +1,69 @@
+import { HttpExceptionFilter } from './exception-filter';
+import { Test } from '@nestjs/testing';
+import { ForbiddenException } from '@nestjs/common';
+
+const mockJson = jest.fn();
+
+const mockStatus = jest.fn().mockImplementation(() => ({
+  json: mockJson,
+}));
+
+const mockGetResponse = jest.fn().mockImplementation(() => ({
+  status: mockStatus,
+}));
+
+const mockHttpArgumentsHost = jest.fn().mockImplementation(() => ({
+  getResponse: mockGetResponse,
+  getRequest: jest.fn().mockImplementation(() => ({
+    url: 'mock-url',
+  })),
+}));
+
+const mockArgumentsHost = {
+  switchToHttp: mockHttpArgumentsHost,
+  getArgByIndex: jest.fn(),
+  getArgs: jest.fn(),
+  getType: jest.fn(),
+  switchToRpc: jest.fn(),
+  switchToWs: jest.fn(),
+};
+
+describe('Http Exception Filter', () => {
+  let httpExceptionFilter: HttpExceptionFilter;
+
+  beforeEach(async () => {
+    const moduleRef = await Test.createTestingModule({
+      providers: [
+        { provide: HttpExceptionFilter, useClass: HttpExceptionFilter },
+      ],
+    }).compile();
+
+    httpExceptionFilter =
+      moduleRef.get<HttpExceptionFilter>(HttpExceptionFilter);
+  });
+
+  it('Instanciado', () => {
+    expect(httpExceptionFilter).toBeDefined();
+  });
+
+  it('Cria um erro corretamente', () => {
+    const erro = new ForbiddenException('teste');
+
+    httpExceptionFilter.catch(erro, mockArgumentsHost);
+
+    expect(mockHttpArgumentsHost).toBeCalledTimes(1);
+    expect(mockHttpArgumentsHost).toBeCalledWith();
+    expect(mockGetResponse).toBeCalledTimes(1);
+    expect(mockGetResponse).toBeCalledWith();
+    expect(mockStatus).toBeCalledTimes(1);
+    expect(mockStatus).toBeCalledWith(erro.getStatus());
+    expect(mockJson).toBeCalledTimes(1);
+    expect(mockJson).toHaveBeenCalledWith(
+      expect.objectContaining({
+        statsCode: erro.getStatus(),
+        path: 'mock-url',
+        message: erro.message,
+      }),
+    );
+  });
+});
