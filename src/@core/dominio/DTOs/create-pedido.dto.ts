968b5b96793d1f00bc2c53e7b87f283c08cbee88
pedroigormarques <pedroigor.marques@gmail.com>
2023-03-04 18:02:50 -0300

    Início da criação do controller dos pedidos

--- src/@core/dominio/DTOs/create-pedido.dto.ts
+++ src/@core/dominio/DTOs/create-pedido.dto.ts
@@ -1,3 +1,4 @@
 export class CreatePedidoDto {
   mesa: number;
+  idUsuario: string;
 }
---
+++ src/controllers/pedido.controller.ts
@@ -0,0 +1,79 @@
+import { PedidoFechado } from './../@core/dominio/pedido-fechado.entity';
+import { Pedido } from './../@core/dominio/pedido.entity';
+import { CreatePedidoDto } from './../@core/dominio/DTOs/create-pedido.dto';
+import { PedidosService } from './../@core/aplicacao/pedidos-service.use-case';
+import { ListaEvento } from './../@core/dominio/lista-evento.entity';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  Sse,
+  UseFilters,
+} from '@nestjs/common';
+import { HttpExceptionFilter } from './../exception/exception-filter';
+import { Observable } from 'rxjs';
+import { HttpCode } from '@nestjs/common/decorators';
+import { HttpStatus } from '@nestjs/common/enums';
+
+@Controller()
+export class UsuarioController {
+  constructor(private readonly pedidoService: PedidosService) {}
+
+  @Sse('pedidos/sse')
+  async carregarEmissorEventos(): Promise<Observable<ListaEvento<Pedido>>> {
+    return this.pedidoService.abrirConexao('idLogado');
+  }
+
+  @Get('pedidos')
+  @UseFilters(HttpExceptionFilter)
+  async carregarPedidos(): Promise<Array<Pedido>> {
+    return await this.pedidoService.carregarPedidos('idLogado');
+  }
+
+  @Post('pedidos')
+  @UseFilters(HttpExceptionFilter)
+  @HttpCode(HttpStatus.CREATED)
+  async adicionarPedido(@Body() dadosPedido: CreatePedidoDto): Promise<Pedido> {
+    return await this.pedidoService.cadastrarPedido('idLogado', dadosPedido);
+  }
+
+  @Get('pedidos/:id')
+  @UseFilters(HttpExceptionFilter)
+  async carregarPedido(@Param() id: string): Promise<Pedido> {
+    return await this.pedidoService.carregarPedido('idLogado', id);
+  }
+
+  @Post('pedidos/:id')
+  @UseFilters(HttpExceptionFilter)
+  async atualizarQtdItemPedido(
+    @Param() id: string,
+    @Body() dadosPedido: { idProdutoCardapio: string; novaQtd: number },
+  ): Promise<Pedido> {
+    return await this.pedidoService.alterarQtdItemDoPedido(
+      'idLogado',
+      id,
+      dadosPedido.idProdutoCardapio,
+      dadosPedido.novaQtd,
+    );
+  }
+
+  @Post('pedidos/:id/deletar')
+  @UseFilters(HttpExceptionFilter)
+  async deletarPedido(@Param() id: string): Promise<void> {
+    return await this.pedidoService.deletarPedido('idLogado', id);
+  }
+
+  @Post('pedidos/:id/fechar')
+  @UseFilters(HttpExceptionFilter)
+  async fecharPedido(@Param() id: string): Promise<PedidoFechado> {
+    return await this.pedidoService.fecharPedido('idLogado', id);
+  }
+
+  @Get('pedidosFechados')
+  @UseFilters(HttpExceptionFilter)
+  async carregarPedidosFechados(): Promise<Array<PedidoFechado>> {
+    return await this.pedidoService.carregarPedidosFechados('idLogado');
+  }
+}
