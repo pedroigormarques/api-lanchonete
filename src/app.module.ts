b7cc5dd39a7e107504275a722d274a474ae54bc4
pedroigormarques <pedroigor.marques@gmail.com>
2023-03-11 12:01:06 -0300

    Dependencias globais transferidas para o AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,11 @@
-import { Module } from '@nestjs/common';
+import { Module, ValidationPipe } from '@nestjs/common';
+import { APP_FILTER, APP_INTERCEPTOR, APP_PIPE } from '@nestjs/core';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AutenticacaoModule } from './autenticacao/autenticacao.module';
+import { ErroDetalhadoEHttpExceptionFilter } from './exception/exception-filter';
+import { MapInterceptor } from './interceptor/map.interceptor';
 import { CardapioModule } from './modulos/cardapio.module';
 import { EstoqueModule } from './modulos/estoque.module';
 import { PedidoModule } from './modulos/pedido.module';
@@ -19,6 +22,20 @@ import { UsuarioModule } from './modulos/usuario.module';
     RepositorioInMemoryModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: MapInterceptor,
+    },
+    {
+      provide: APP_FILTER,
+      useClass: ErroDetalhadoEHttpExceptionFilter,
+    },
+    {
+      provide: APP_PIPE,
+      useValue: new ValidationPipe({ whitelist: true, transform: true }),
+    },
+  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,9 @@
-import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app.module';
-import { ErroDetalhadoEHttpExceptionFilter } from './exception/exception-filter';
-import { MapInterceptor } from './interceptor/map.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalFilters(new ErroDetalhadoEHttpExceptionFilter());
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
-  app.useGlobalInterceptors(new MapInterceptor());
-
   await app.listen(3000);
 }
 bootstrap();
